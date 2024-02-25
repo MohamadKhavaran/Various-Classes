abd88d7b0f759eb9ae59105627dced95f93e95a5
MohamadKhavaran <133672393+MohamadKhavaran@users.noreply.github.com>
2024-02-25 09:58:02 +0330

    Add Scaled Class

---
+++ Various Classes/Various Classes/ScaledClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Various_Classes
+{
+    //A class that cannot be inherited
+    sealed internal class ScaledClass
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
